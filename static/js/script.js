87d43431456699c2a01f8a53a4de51ef7a50fd33
Willian <williansilvap25@gmail.com>
2022-06-18 17:10:59 -0300

    navbar ok

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+//////////////////////////////////////////////////
+
 var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function () {
@@ -8,26 +10,36 @@ window.addEventListener('scroll', function () {
     }
 });
 
-//////////
+//////////////////////////////////////////////////
 
 const hamburgerMenu = document.querySelector('.hamburgerMenu');
 const body = document.querySelector('body');
-const navMenu = document.querySelector('.navbar-nav');
+const navMenu = document.querySelector('.navMenu');
 
 hamburgerMenu.addEventListener("click", () => {
     hamburgerMenu.classList.contains("bi-list")
         ? hamburgerMenu.classList.replace("bi-list", "bi-x")
         : hamburgerMenu.classList.replace("bi-x", "bi-list");
-    //navMenu.classList.toggle("active"); // adiciona a classe .navMenu e abre a navbar mobile
-    //body.classList.toggle("menuNavActive"); //adicionando a classe no body
+
+    navMenu.classList.toggle("active");
+
+    navMenu.classList.contains("active")
+        ? nav.classList.add('bg-dark', 'shadow')
+        : nav.classList.remove('bg-dark', 'shadow');
+
+    body.classList.toggle("menuNavActive");
+
+    navMenu.classList.contains("active")
+        ? body.classList.add('menuNavActive')
+        : body.classList.remove('menuNavActive');
 });
 
-/*
-document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => { // quando há um clique nos links da nav mobile
-    hamburgerMenu.classList.remove("bi-x"); // remove o ícone x
-    hamburgerMenu.classList.add("bi-list") // adiciona o ícone list
-    navMenu.classList.remove("active"); // remova a classe ativa da classe .navMenu
-    //if (document.querySelector("menuNavActive"))
-    //body.classList.toggle("menuNavActive"); //remove a classe no body
+document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("bi-x");
+    hamburgerMenu.classList.add("bi-list");
+    navMenu.classList.remove("active");
+    body.classList.remove("menuNavActive");
+    this.window.pageYOffset > 56
+        ? nav.classList.add('bg-dark', 'shadow')
+        : nav.classList.remove('bg-dark', 'shadow');
 }));
-*/
