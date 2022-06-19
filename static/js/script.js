adda7e8992f7a654817ba43713658b2488650c2f
Willian <williansilvap25@gmail.com>
2022-06-19 16:14:46 -0300

    navlinks js

--- static/js/script.js
+++ static/js/script.js
@@ -43,3 +43,48 @@ document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", (
         ? nav.classList.add('bg-dark', 'shadow')
         : nav.classList.remove('bg-dark', 'shadow');
 }));
+
+//////////////////////////////////////////////////
+
+const menuLinks = document.querySelectorAll('.navbar a[href^="#"], .arrasteInicio[href^="#"]');
+
+function getDistanceFromTheTop(element) {
+    const id = element.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const distanceFromTheTop = getDistanceFromTheTop(event.target) - 70;
+    smoothScrollTo(0, distanceFromTheTop, 200)
+}
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+});
+
+function smoothScrollTo(endX, endY, duration) {
+    const startX = window.scrollX || window.pageXOffset;
+    const startY = window.scrollY || window.pageYOffset;
+    const distanceX = endX - startX;
+    const distanceY = endY - startY;
+    const startTime = new Date().getTime();
+
+    duration = typeof duration !== "undefined" ? duration : 400;
+
+    const easeInOutQuart = (time, from, distance, duration) => {
+        if ((time /= duration / 2) < 1)
+            return (distance / 2) * time * time * time * time + from;
+        return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+    };
+
+    const timer = setInterval(() => {
+        const time = new Date().getTime() - startTime;
+        const newX = easeInOutQuart(time, startX, distanceX, duration);
+        const newY = easeInOutQuart(time, startY, distanceY, duration);
+        if (time >= duration) {
+            clearInterval(timer);
+        }
+        window.scroll(newX, newY);
+    }, 1000 / 60);
+}
