ea639b729393750949a220fde014d0bbd73599fd
Willian <williansilvap25@gmail.com>
2022-06-21 23:09:42 -0300

    animations

--- static/js/script.js
+++ static/js/script.js
@@ -88,3 +88,25 @@ function smoothScrollTo(endX, endY, duration) {
         window.scroll(newX, newY);
     }, 1000 / 60);
 }
+
+//////////////////////////////////////////////////
+
+const item = document.querySelectorAll("[data-anime");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate")
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+});
